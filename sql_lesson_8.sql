c9c17d5b9272ae62a05b67991985e254fcea0701
YuKorostelev <99680778+YuKorostelev@users.noreply.github.com>
2022-03-08 17:12:16 +0300

    Add files via upload

---
+++ sql_lesson_8.sql
@@ -0,0 +1,82 @@
+--task1  (lesson8)
+-- oracle: https://leetcode.com/problems/department-top-three-salaries/
+
+--task2  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/17
+
+SELECT fm.member_name, fm.status, SUM(p.amount*p.unit_price) as costs
+FROM FamilyMembers as fm
+JOIN Payments as p ON p.family_member = fm.member_id
+WHERE YEAR(p.date) = 2005
+GROUP BY fm.member_id, fm.member_name, fm.status
+
+--task3  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/13
+
+select name
+from Passenger
+GROUP BY name 
+having COUNT(name) > 1
+
+--task4  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/38
+
+select count(*) as Count
+from student 
+where first_name = 'Anna'
+
+
+--task5  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/35
+
+--task6  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/38
+
+
+select count(*) as Count
+from student 
+where first_name = 'Anna'
+
+--task7  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/32
+
+SELECT FLOOR(avg(YEAR(CURRENT_DATE) - YEAR(birthday))) AS age
+FROM FamilyMembers
+
+Ни FLOOR, ни ceil не подошли
+
+
+--task8  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/27
+
+--task9  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/37
+
+SELECT min(timestampdiff(YEAR ,birthday, current_date)) as year
+from Student
+
+--task10  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/44
+
+select max(TIMESTAMPDIFF(year, birthday, current_date)) as max_year
+FROM Student
+JOIN Student_in_class
+on Student_in_class.student = Student.id
+join Class
+on Student_in_class.class = class.id
+where class.name like '10%'
+
+--task11 (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/20
+
+--task12  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/55
+
+--task13  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/45
+
+--task14  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/43
+
+--task15  (lesson8)
+-- https://sql-academy.org/ru/trainer/tasks/63
