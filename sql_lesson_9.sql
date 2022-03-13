168d86c7f7ca64985d72696353c796067e52a7f9
YuKorostelev <99680778+YuKorostelev@users.noreply.github.com>
2022-03-13 20:54:48 +0300

    Update sql_lesson_9.sql

--- sql_lesson_9.sql
+++ sql_lesson_9.sql
@@ -55,3 +55,9 @@ ORDER BY employee_id;
 
 --task8  (lesson9)
 -- oracle: https://www.hackerrank.com/challenges/the-report/problem
+SELECT 
+CASE WHEN grd.grade < 8 THEN NULL 
+WHEN grd.grade >= 8 THEN std.name END,
+grd.grade, std.marks FROM students std, grades grd
+WHERE std.marks BETWEEN grd.min_mark AND grd.max_mark
+ORDER BY grd.grade DESC, std.name ASC;
